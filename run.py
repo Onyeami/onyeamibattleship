13b33e3824b861ef267b3cd449230f5285c9a74e
Onyeami <>
2023-10-08 14:33:58 +0000

    created function to print the grid in a nice format

--- run.py
+++ run.py
@@ -1,8 +1,16 @@
 # Import the random module to generate random
 import random
 
+
 # Function to create a grid of given size filled with 'o'
 def create_grid(size):
     return [['o'] * size for _ in range(size)]
+
+
+# Function to print the grid in a nice format
+def print_grid(grid):
+    size = len(grid)
+    print("  " + " ".join([chr(65+i) for i in range(size)])) # Print column headers (A, B, C, ...)
+    print("  " + "-" * (2*size+1)) # print line separator
 
 
